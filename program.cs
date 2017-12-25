5633e26d5d8178901f57a0ec9a158b2cccdf2c05
Chessunder <Andrew4esnokov@gmail.com>
2017-12-25 15:41:08 +0500

    fffgfggfff444

--- program.cs
+++ program.cs
@@ -12,6 +12,8 @@ namespace HelloWorld
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
+
+            
         }
     }
 }
