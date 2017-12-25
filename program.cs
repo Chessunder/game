927c8885923a3e224b80397f3c947bf600f10212
Chessunder <Andrew4esnokov@gmail.com>
2017-12-25 16:16:47 +0500

    dd

--- program.cs
+++ program.cs
@@ -12,6 +12,9 @@ namespace HelloWorld
             Console.WriteLine("Press any key to exit.");
             dsfjsdfhisd s2 2 ;
             Console.ReadKey();
+
+
+            
         }
     }
 }
