cdceda29f958981d3d07cfaf25b8b2dcdf51c3f6
Chessunder <Andrew4esnokov@gmail.com>
2017-12-25 15:50:58 +0500

    prog 2 add

---
+++ program2.cs
@@ -0,0 +1,17 @@
+// A Hello World! program in C#.
+using System;
+
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+            // Keep the console window open in debug mode.
+            int a = 2 + 2;
+            Console.WriteLine("{0}Press any key to exit.",a);
+            Console.ReadKey();
+        }
+    }
+}
